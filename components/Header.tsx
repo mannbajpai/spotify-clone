983026efcbf8313f60aad367ee6a74b9cab607fb
mannbajpai <mannbajpai.grs@gmail.com>
2023-06-23 01:56:22 +0530

    Songs Upload to Supabase

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -14,6 +15,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const router = useRouter();
+  const authModal = useAuthModal();
 
   const handleLogout = () => {
     // handling logout
@@ -90,7 +92,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           <>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={authModal.onOpen}
                 className="
             bg-transparent
             text-neutral-300
@@ -101,7 +103,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             </div>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={authModal.onOpen}
                 className="bg-white px-6 py-2 text-black"
               >
                 Login
